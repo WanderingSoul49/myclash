1175c6aae1859be1dd82e68d82d63e3ef91f4917
zhongyuming <puppetdev@163.com>
2023-07-11 17:01:27 +0800

    Auto Pushed By Python Scripts at 2023-07-11 17:01:27

--- parser.js
+++ parser.js
@@ -18,27 +18,74 @@ module.exports.parse = async (
   const proxyGroups = [];
 
   // 给 proxy-groups 添加一个策略组，过滤掉带有特定字样的节点
-  const notIncludedFully = [];
-  const notIncludedPartly = ["香港", "日本", "圣何塞", "印度", "首尔", "新加坡", "美国"];
-  const filteredProxies4AI = proxies.filter(
-    (proxy) =>
-      notIncludedFully.indexOf(proxy) === -1 &&
-      notIncludedPartly.every((keyword) => !proxy.includes(keyword))
-  );
+  const notIncludedFully = ["德国-15"];
+  const notIncludedPartly = [
+    "香港",
+    "日本",
+    "圣何塞",
+    "印度",
+    "首尔",
+    "美国",
+    "春川",
+  ];
+  // have the highest priority. if onlyIncluded is not none, then just use the node of onlyIncluded
+  const onlyIncluded = ["新加坡"];
+  // the filter result array
+  let filteredProxies4AI = [];
+  // judge whether the onluInclude is empty by length.
+  if (onlyIncluded.length > 0) {
+    filteredProxies4AI = proxies.filter(
+      (proxy) => onlyIncluded.indexOf(proxy) !== -1
+    );
+  } else {
+    filteredProxies4AI = proxies.filter(
+      (proxy) =>
+        notIncludedFully.indexOf(proxy) === -1 &&
+        notIncludedPartly.every((keyword) => !proxy.includes(keyword))
+    );
+  }
+  console.log("The filter result is ", filteredProxies4AI);
 
   // push the proxy group for ai website
-  proxyGroups.push({
-    name: "URL-TEST-AI",
-    type: "url-test",
-    url: "http://www.gstatic.com/generate_204",
-    interval: 300,
-    lazy: true,
-    tolerance: 50,
-    proxies: filteredProxies4AI,
-  });
+  proxyGroups.push(
+    {
+      name: "AI",
+      type: "select",
+      proxies: ["URL-TEST-AI", "SELECT-AI"],
+    },
+    {
+      name: "URL-TEST-AI",
+      type: "url-test",
+      url: "http://www.gstatic.com/generate_204",
+      interval: 300,
+      lazy: true,
+      tolerance: 50,
+      proxies: filteredProxies4AI,
+    },
+    {
+      name: "SELECT-AI",
+      type: "select",
+      proxies: filteredProxies4AI,
+    }
+  );
 
   // push other my custom proxy groups.
   proxyGroups.push(
+    {
+      name: "PROXY",
+      type: "select",
+      proxies: ["URL-TEST", "LOAD-BALANCE", "SELECT", "DIRECT"],
+    },
+    {
+      name: "OTHER", //规则未命中
+      type: "select",
+      proxies: ["PROXY", "DIRECT"],
+    },
+    {
+      name: "AD",
+      type: "select",
+      proxies: ["REJECT", "DIRECT", "PROXY"],
+    },
     {
       name: "URL-TEST",
       type: "url-test",
@@ -59,24 +106,8 @@ module.exports.parse = async (
       name: "SELECT",
       type: "select",
       proxies: proxies,
-    },
-    {
-      name: "PROXY",
-      type: "select",
-      proxies: ["URL-TEST", "LOAD-BALANCE", "SELECT", "DIRECT"],
-    },
-    {
-      name: "OTHER", //规则未命中
-      type: "select",
-      proxies: ["PROXY", "DIRECT"],
-    },
-    {
-      name: "AD",
-      type: "select",
-      proxies: ["REJECT", "DIRECT", "PROXY"],
     }
   );
-
 
   // final result
   let result = {
@@ -85,17 +116,29 @@ module.exports.parse = async (
   };
 
   // set rules and rule providers
-  const urlRule =
-    "https://gist.githubusercontent.com/puppetdevz/ba94db6d192908bda07b8fb43e63cb24/raw/9e775767ae45fe0e60e65f65dddd055c042f4e88/remote_rules.yml";
-  const { status, data } = await axios.get(urlRule);
-  if (status !== 200) {
-    console.log("error " + status);
+  // const urlRule =
+  //   "https://gist.githubusercontent.com/puppetdevz/ba94db6d192908bda07b8fb43e63cb24/raw/9e775767ae45fe0e60e65f65dddd055c042f4e88/remote_rules.yml";
+  // const { status, ruleData } = await axios.get(urlRule);
+  // if (status !== 200) {
+  //   console.log("error " + status);
+  //   return yaml.stringify(result);
+  // }
+  const fs = require("fs");
+  const path = require("path");
+  let ruleData;
+  try {
+    // console.log(__dirname);
+    ruleData = fs.readFileSync(path.join(__dirname, "rules.yaml"), "utf8");
+    // console.log(ruleData);
+  } catch (err) {
+    console.error(err);
     return yaml.stringify(result);
   }
-  const ruleObj = yaml.parse(data);
-  console.log(ruleObj);
 
-  result.rules = ruleObj["prepend-rules"]
-  result["rule-providers"] = ruleObj["mix-rule-providers"]
+  const ruleObj = yaml.parse(ruleData);
+  // console.log(ruleObj);
+
+  result.rules = ruleObj["prepend-rules"];
+  result["rule-providers"] = ruleObj["mix-rule-providers"];
   return yaml.stringify(result);
 };
